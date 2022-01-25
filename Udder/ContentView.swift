d1c4a3c003f2a8fec9d43270ea2210346dce7fc9
2b-alpha <2b.alpha20@gmail.com>
2022-01-25 16:55:23 +0900

    추가하였사옵니다 ㅎㅎ

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Text("hohoho~")
+            .font(.system(size: 40))
+            .bold()
+            
     }
 }
 
